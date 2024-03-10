4ce3417bc4b5c4ae3f281097479ac6d9aa03d728
fevoritos <fevoritos@mail.ru>
2024-03-11 00:03:22 +0300

    static and images

--- app/main.py
+++ app/main.py
@@ -1,18 +1,35 @@
-from fastapi import FastAPI, Query, Depends
-from datetime import date
-from pydantic import BaseModel
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.bookings.router import router as router_bookings
 from app.users.router import router as router_users
 from app.hotels.rooms.router import router as router_rooms
 
 from app.pages.router import router as router_pages
+from app.images.router import router as router_images
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="app/static"), "static")
 
 app.include_router(router_users)
 app.include_router(router_bookings)
 app.include_router(router_rooms)
 
 app.include_router(router_pages)
+app.include_router(router_images)
 
+origins = [
+    "http:lacalhost:3000",
+
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", 
+                   "Access-Control-Allow-Origin", "Authorization"],
+)
